980c6f32c31c3f8934498905d633d920d4dc541a
senorindependiente <pingelchristian@hotmail.com>
2022-02-25 12:06:08 +0100

    Setting up MainView component

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Mainview } from "./components/main-view/main-view";
+import MainView from "./components/main-view/main-view";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
@@ -8,7 +8,11 @@ import "./index.scss";
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return (<MainView />)
+    return (
+      <div className="my-flix">
+        <MainView />
+      </div>
+    );
   }
 }
 
